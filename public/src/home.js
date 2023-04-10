96f510708af357623529c80c831c1b1ba0946d1f
Mariam Leggon <mariamthedeveloper@gmail.com>
2023-04-10 12:33:16 -0400

    added reduce

--- public/src/home.js
+++ public/src/home.js
@@ -9,57 +9,61 @@ function getTotalAccountsCount(accounts) {
 function getBooksBorrowedCount(books) {
   let booksCheckedOut = books.filter(
     (book) =>
-     book.borrows.filter((status) => status.returned === false).length > 0
-   );
-   return booksCheckedOut.length;
+      book.borrows.filter((status) => status.returned === false).length > 0
+  );
+  return booksCheckedOut.length;
 }
 
 function getMostCommonGenres(books) {
-  let commonGenres = {};
-  books.forEach((book)=> {
+  const commonGenres = books.reduce((genres, book) => {
     const genre = book.genre;
-    if(commonGenres[genre]){
-     commonGenres[genre]++ 
-    }else{
-      commonGenres[genre]=1
+    if (genres[genre]) {
+      genres[genre]++;
+    } else {
+      genres[genre] = 1;
     }
-    
-  })
-  console.log(commonGenres)
-  let newArray = []
-  for(let name in commonGenres){
-   newArray.push({name, count:commonGenres[name]}) 
+    return genres;
+  }, {});
+
+  console.log(commonGenres);
+  let newArray = [];
+  for (let name in commonGenres) {
+    newArray.push({ name, count: commonGenres[name] });
   }
-  return newArray.sort((a,b)=> b.count -a.count).slice(0,5)
+  return sortAndSlice(newArray)
+  
 }
 
 function getMostPopularBooks(books) {
-  return books
-  .map((book) => {
-   return { name: book.title, count: book.borrows.length };
-  })
-  .sort((a, b) => (a.count < b.count ? 1 : -1))
-  .slice(0, 5);
+  const result = books
+    .map((book) => {
+      return { name: book.title, count: book.borrows.length };
+    })
+    return sortAndSlice(result)
+   
 }
 
 function getMostPopularAuthors(books, authors) {
   let result = [];
   authors.forEach((author) => {
-   let theAuthor = {
-    name: `${author.name.first} ${author.name.last}`,
-    count: 0
-   };
-   books.forEach((book) => {
-    if (book.authorId === author.id) {
-     theAuthor.count += book.borrows.length;
-    }
-   });
-   result.push(theAuthor);
+    let theAuthor = {
+      name: `${author.name.first} ${author.name.last}`,
+      count: 0,
+    };
+    books.forEach((book) => {
+      if (book.authorId === author.id) {
+        theAuthor.count += book.borrows.length;
+      }
+    });
+    result.push(theAuthor);
   });
-  return result.sort((a, b) => b.count - a.count).slice(0, 5);
+  
+ return sortAndSlice(result)
 }
- 
 
+function sortAndSlice(array){
+  return array.sort((a, b) => b.count - a.count).slice(0, 5);
+}
 module.exports = {
   getTotalBooksCount,
   getTotalAccountsCount,
